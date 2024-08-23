25f3a2c39aa11e7e1fa2601ae430bc39da80793c
febinfranco <febinfranco2015@gmail.com>
2024-08-23 18:22:39 +0530

    Add files via upload

---
+++ parameters.m
@@ -0,0 +1,25 @@
+%% parameters for DG
+pra=1.1e3;
+vra=62.5;
+vdc=200;
+cdcc=220e-6;
+f0=50;
+fsw=20e3;
+lf=1.5e-3;
+cf=20e-6;
+rf=1.5e-3;
+p0=240;
+q0=60;
+iodmax=12;
+iqdmax=-7.2;
+md=0.001;
+nq=0.002;
+rl=0.001;
+ll=0.001e-3;
+wlpf=15;
+wc=2*pi*cf;
+%% pi controller
+kpid=0.4;
+kiid=0.5;
+kpiq=-4;
+kiiq=-5;
